6bfa9ef3381a2f83b31406e727e4dc6d93052aa1
咸鱼c <542225712@qq.com>
2021-06-04 17:56:34 +0800

    feat: add hook

---
+++ src/utils/index.js
@@ -0,0 +1,37 @@
+import { useEffect, useRef, useState } from "react";
+export const isVoid = (value) =>
+  value === undefined || value === null || value === "";
+
+export const cleanObject = (object) => {
+  if (!object) {
+    return {};
+  }
+  const result = { ...object };
+  Object.keys(result).forEach((key) => {
+    const value = result[key];
+    if (isVoid(value)) {
+      delete result[key];
+    }
+  });
+  return result;
+};
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+};
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    // 每次在value变化以后，设置一个定时器
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    // 每次在上一个useEffect处理完以后再运行
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
