5b4099f341b76f76f758400640534f67ba6d58f3
咸鱼c <542225712@qq.com>
2021-06-06 18:54:25 +0800

    feat: 添加全局状态管理

---
+++ src/context/auth.context.tsx
@@ -0,0 +1,33 @@
+import React, { ReactNode, useState } from 'react';
+import { User } from '../screens/projectlist/searchpanel'
+import * as auth from '../auth-provider';
+
+interface AuthForm {
+  username: string,
+  password: string
+}
+
+const AuthContext = React.createContext<{
+  user: User | null,
+  register: (form: AuthForm) => Promise<void>,
+  login: (form: AuthForm) => Promise<void>,
+  logout: () => Promise<void>,
+} | undefined>(undefined);
+AuthContext.displayName = 'AuthContext'; 
+
+export const AuthProvider = ({children}: {children: ReactNode}) => {
+  const [user, setUser] = useState<User | null>(null);
+  const login = (form: AuthForm) => auth.login(form).then(user => setUser(user));
+  const register = (form: AuthForm) => auth.register(form).then(user => setUser(user));
+  const logout = () => auth.logout().then(() => setUser(null));
+  // 为什么需要 children的类型？因为标签里面的子组件就相当于父节点加了 children属性，属性内容就是子节点
+  return <AuthContext.Provider children={children} value={{user, login, register, logout}} />
+}
+
+export const useAuth = () => {
+  const context = React.useContext(AuthContext);
+  if(!context) {
+    throw new Error('useAuth必须在AuthProvider中使用')
+  }
+  return context
+}
